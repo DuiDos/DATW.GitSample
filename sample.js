31eab3cd510ce2ee0c32df12c0a3515a38689b6a
duber <besanson@hotmail.com>
2024-11-08 13:47:06 +0100

    add sample.js changings

--- sample.js
+++ sample.js
@@ -1,2 +1,14 @@
 console.log("Hola Mundo")
 console.log("Hola Mundo 2")
+console.log("Hola Mundo 3")
+
+const user = 'Manolín';
+
+console.log('El contenido de la variable name: ' + user);
+
+function rumiPunki () {
+const punki = querySelector('form button')
+const rumi = quweySelector('form button')
+const miau = querySelctor('form button')
+
+}
